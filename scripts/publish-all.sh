a328fabbdfc08a20ceda7d7b6a0a7e8740d48ec9
Marek Czajkowski <marek.czajkowski@ds.pl>
2026-03-30 15:07:51 +0200

    Puresight Demo mesh and resources

--- scripts/publish-all.sh
+++ scripts/publish-all.sh
@@ -2,7 +2,8 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 pushd "${SCRIPT_DIR}/../" || exit
 
 streamx publish events data
-streamx publish stream data/catalog/products.stream
 streamx publish stream data/catalog/categories.stream
+streamx publish stream data/catalog/products.stream
+streamx publish stream data/download-requests/scheduled-downloads.stream
 
 popd || exit
